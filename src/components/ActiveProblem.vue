0753c23339b879a830f0a3c7b29be8fc3da70d5d
Gosten <40144490+Gosten@users.noreply.github.com>
2022-11-13 20:54:16 +0100

    Update from main repo

--- src/components/ActiveProblem.vue
+++ src/components/ActiveProblem.vue
@@ -3,7 +3,7 @@
     <div class="active-problem-top-container">
       <button class="back-to-load-problem-btn" @click="goToLoadProblem">
         <i class="mdi mdi-arrow-left"></i>
-        <!-- <p class="back-to-load-problem-btn-text"> -->powrót</p>
+        <!-- <p class="back-to-load-problem-btn-text">powrót</p> -->
       </button>
     </div>
     <div id="board-style" class="width-100 flex-container-blank">
