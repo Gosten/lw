08ebfb8cdc4b41fb6fbef84c5f8224940192013b
Gosten <40144490+Gosten@users.noreply.github.com>
2022-12-03 13:21:15 +0100

    Test change header font and background color

--- src/js/utils.js
+++ src/js/utils.js
@@ -41,7 +41,7 @@ function limitNameLength(name, length = 15) {
 
 function parseName(problem) {
   //connect problem name with author
-  const shortName = limitNameLength(problem.name, 10);
+  const shortName = limitNameLength(problem.name, 16);
   if (problem.author && ENABLE_AUTHOR)
     return `${shortName} (${problem.author})`;
   return limitNameLength(problem.name, 15);
